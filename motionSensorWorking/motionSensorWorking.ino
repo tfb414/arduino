69ae8976a6691f0a1657b2902e38f7db9a36d64d
Tim Brady <tfb414@gmail.com>
2019-08-27 14:44:51 -0400

    rewrite

--- motionSensorWorking/motionSensorWorking.ino
+++ motionSensorWorking/motionSensorWorking.ino
@@ -4,19 +4,16 @@
 #include <ArduinoJson.h>
 #include "secrets/motion_sensor_s3cr3ts.h"
 
-#define timeSeconds 10
-
 const int sensePin = 36;
 const int ledPin = 21;
-
-unsigned long now = millis();
-unsigned long lastTrigger = 0;
-boolean startTimer = false;
 
 char ssid[] = SECRET_SSID;
 char pass[] = SECRET_PASS;
 
 char homeAssistantId[] = SECRET_HOME_ASSISTANT_ID;
+
+int pirState = LOW;
+int pirValue = 0;      
 
 void connectToWifi()
 {
@@ -26,6 +23,7 @@ void connectToWifi()
 
 void toggleOnLight()
 {
+  Serial.println("toggleOnLight");
   HTTPClient http;
   String url = "http://192.168.1.69:3000/motionSensorActivated";
 
@@ -47,57 +45,34 @@ void toggleOnLight()
   http.end();
 }
 
-void IRAM_ATTR detectsMovement()
-{
-  Serial.println("MOTION DETECTED!!!");
-  digitalWrite(ledPin, HIGH);
-  startTimer = true;
-  lastTrigger = millis();
-}
-
 void setup()
 {
   Serial.begin(9600);
-  pinMode(sensePin, INPUT_PULLUP);
+  pinMode(sensePin, INPUT);
   pinMode(ledPin, OUTPUT);
 
-  attachInterrupt(digitalPinToInterrupt(sensePin), detectsMovement, RISING);
-  //  pinMode(sensePin, INPUT);
-
-  //  while (WiFi.status() != WL_CONNECTED)
-  //    {
-  //      connectToWifi();
-  //      delay(2000);
-  //    }
-  //  Serial.print("Connected at: ");
-  //  Serial.println(WiFi.localIP());
+  while (WiFi.status() != WL_CONNECTED)
+      {
+        connectToWifi();
+        delay(3000);
+      }
+     Serial.println("connected");
 }
-
-void loop()
-{
-  now = millis();
-  if (startTimer && (now - lastTrigger > (timeSeconds * 1000)))
-  {
-    Serial.println("Motion stopped...");
-    digitalWrite(ledPin, LOW);
-    startTimer = false;
+ 
+void loop(){
+  pirValue = digitalRead(sensePin);
+  if (pirValue == HIGH) {            
+    digitalWrite(ledPin, HIGH);  
+    if (pirState == LOW) {
+      toggleOnLight();
+      Serial.println("Motion detected!");
+      pirState = HIGH;
+    }
+  } else {
+    digitalWrite(ledPin, LOW); 
+    if (pirState == HIGH){
+      Serial.println("Motion ended!");
+      pirState = LOW;
+    }
   }
-  //  if(WiFi.status() == WL_CONNECTED){
-
-  //  int motionSensorValue = analogRead(sensePin);
-  //  Serial.println(motionSensorValue);
-  //  if (motionSensorValue > 0 )
-  //  {
-  //    digitalWrite(ledPin, HIGH);
-  //        delay(5000);
-  //        toggleOnLight();
-  //        digitalWrite(ledPin, LOW);
-  //    Serial.println("just sent request");
-  //        delay(5000);
-  //  }
-  //  else
-  //  {
-  //    digitalWrite(ledPin, LOW);
-  //  }
-  //    }
 }
