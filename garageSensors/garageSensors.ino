f72085bbb5ed3ab48ebebaac0ab1fc39637a28ac
Tim Brady <timbrady@Tims-MacBook-Pro.local>
2020-09-06 17:14:07 -0600

    Fix parse of Json

--- garageSensors/garageSensors.ino
+++ garageSensors/garageSensors.ino
@@ -13,8 +13,8 @@ char pass[] = SECRET_PASS;
 const size_t capacity = JSON_OBJECT_SIZE(2) + 65;
 DynamicJsonDocument homeStatus(capacity);
 
-char garageDoorClosed;
-char garageLightOff;
+bool garageDoorClosed;
+bool garageLightOff;
 
 void connectToWifi()
 {
@@ -45,8 +45,8 @@ void updateGarageStatus(String garageDoorClosedParam, String garageLightOffParam
     
     DeserializationError error = deserializeJson(responseJson, response);
     homeStatus = responseJson;
-//    garageDoorClosed = homeStatus["garageDoorClosed"];
-//    garageLightOff = homeStatus["garageLightOff"];
+    garageDoorClosed = homeStatus["garageDoorClosed"];
+    garageLightOff = homeStatus["garageLightOff"];
   }
   http.end();
 
@@ -72,6 +72,7 @@ void getGarageStatus()
     Serial.print("Response code: ");
     Serial.println(httpResponseCode);
     Serial.print("REsponse: ");
+    Serial.println(response);
 
     DeserializationError error = deserializeJson(responseJson, response);
     if (error) {
@@ -79,12 +80,15 @@ void getGarageStatus()
     Serial.println(error.c_str());
     return;
   }
+    
     homeStatus = responseJson;
-    const char garageDoorClosed = homeStatus["garageDoorClosed"];
-    const char garageLightOff = homeStatus["garageLightOff"];
-
-    Serial.print(garageDoorClosed);
-    Serial.println("HEY OVER HERE");
+//    const bool garageDoorClosed = homeStatus["garageDoorClosed"];
+//    const bool garageLightOff = homeStatus["garageLightOff"];
+//
+//    Serial.print(garageDoorClosed);
+//    Serial.println("HEY OVER HERE");
+//    Serial.println(garageLightOff);
+//    Serial.println("did that work?");
 
   }
   http.end();
@@ -102,59 +106,32 @@ void setup()
         connectToWifi();
         delay(3000);
       }
-     Serial.println("connected");
+  Serial.println("connected");
 
-    getGarageStatus();
-    garageDoorClosed = homeStatus["garageDoorClosed"];
-    garageLightOff = homeStatus["garageLightOff"];
-//    const char* test = homeStatus["garageDoor"]
-
-//   const char* garageDoor = homeStatus["garageDoor"];
-//   const char* garageLight = homeStatus["garageLight"];
-//   Serial.print(garageDoor);
-//   Serial.print(garageLight);
-
-//  updateGarageStatus("true", "false");
+  getGarageStatus();
+    
   garageDoorClosed = homeStatus["garageDoorClosed"];
   garageLightOff = homeStatus["garageLightOff"];
-  Serial.print(garageDoorClosed);
-  Serial.print(garageLightOff);
+  Serial.print("garageDoorClosed: ");
+  Serial.println(garageDoorClosed);
+  Serial.print("garageLightOff: ");
+  Serial.println(garageLightOff);
+  
 }
  
 void loop(){
   int garageDoorSense = digitalRead(garageDoorPin);
   int lightSense = analogRead(lightSensorPin);
 
-//  bool garageDoorClosed = garageDoorSense < 0; // true === closed
-//  bool garageLightOff = lightSense < 500; // true === off 
+  bool newGarageDoorClosed = garageDoorSense < 0; 
+  bool newGarageLightOff = lightSense < 500;
 
+ 
+  if (newGarageDoorClosed != homeStatus["garageDoorClosed"] || newGarageLightOff != homeStatus["garageLightOff"]) {
+    getGarageStatus();
 
-  garageDoorClosed = homeStatus["garageDoorClosed"];
-  garageLightOff = homeStatus["garageLightOff"];  
-
-   bool garageDoorClosedLocal = true;
-   bool garageLightOffLocal = true;
-
-   Serial.print("garageDoorClosed");
-  Serial.println(garageDoorClosed);
-
-  Serial.print("garageLightOff");
-  Serial.println(garageLightOff);
-
-
-//  bool testGarage = garageDoorClosed != homeStatus["garageDoorClosed"];
-//  bool testLight = garageLightOff != homeStatus["garageLightOff"];
-//
-//  Serial.print("boolean");
-//  Serial.println(testGarage);
-//
-//  Serial.print("current light value");
-//  Serial.println(testLight);
-//  
-//  
-//  if (garageDoorClosed != homeStatus["garageDoorClosed"] || garageLightOff != homeStatus["garageLightOff"]) {
-//    getGarageStatus();
-//
-//    updateGarageStatus("true", "true");
-//  }
+    updateGarageStatus("true", "true");
+    getGarageStatus();
+    
+  }
 }
